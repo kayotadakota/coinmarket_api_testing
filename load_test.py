b4a1f0644099c5050dc374b34c57eb4fd45520b6
Kayota <kayota1992@yandex.ru>
2020-11-14 14:16:28 +0300

    change load_test.py

--- load_test.py
+++ load_test.py
@@ -1,4 +1,5 @@
 import os
+import math
 import time
 import queue
 import requests
@@ -6,6 +7,12 @@ import datetime
 import threading
 
 queue_results = queue.Queue()
+
+def percentile(lst, percent):
+    if not lst:
+        return None
+    index = math.floor(len(lst) * percent)
+    return lst[int(index)]
 
 def get_request():
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?'
@@ -21,18 +28,15 @@ def get_request():
 
     return response.status_code, response.elapsed.total_seconds()
 
-def loop():
-    for i in range(1):
-        test_result, elapsed_time = get_request()
-        queue_results.put([test_result, elapsed_time])
-
 def test_rps():
     concurrent_user = 8
     workers = []
     start_time = time.time()
 
     for i in range(concurrent_user):
-        thread = threading.Thread(target=loop, daemon=True)
+        test_result, elapsed_time = get_request()
+        thread = threading.Thread(args=(test_result, elapsed_time), daemon=True)
+        queue_results.put([test_result, elapsed_time])
         thread.start()
         workers.append(thread)
 
@@ -46,10 +50,12 @@ def test_rps():
     total_pass_requests = 0
     qsize = queue_results.qsize()
     latency = 0.8 # 80%
+    data = []
 
     for i in range(qsize):
         try:
             result = queue_results.get_nowait()
+            data.append(round(result[1], 3))
         except Empty:
             break
         if result[0] == 200:
@@ -57,8 +63,7 @@ def test_rps():
 
     tested_time = end_time - start_time
     rps = total_pass_requests / tested_time
-    delay = datetime.timedelta(milliseconds=tested_time * latency)
 
     assert concurrent_user == total_pass_requests
     assert rps > 5
-    assert delay < reference
+    assert percentile(sorted(data), latency) < 450
